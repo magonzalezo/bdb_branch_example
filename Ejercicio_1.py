aad15f6cc997214537eca4388cf6fc848a075e88
Miguel <miguel.gonzalez.ordonez@gmail.com>
2020-08-02 15:47:51 -0500

    Change a label value

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,5 +1,5 @@
 '''
-Ejercicio 6: Mostrar todaslas tablas de multiplicar del 1 al 10.
+Ejercicio 1: Mostrar todaslas tablas de multiplicar del 1 al 10.
              Mostrando el titulo de la tabla y luego las multiplicaciones del 1 al 10.
 '''
 
